b1e38f4301dcacd2030d24a2845c431dd2cfc66f
Abhilash <panaroma365@gmail.com>
2024-10-08 19:41:17 +0530

    added logging dependencies

--- src/main/java/mindsdb/Main.java
+++ src/main/java/mindsdb/Main.java
@@ -1,17 +1,6 @@
 package mindsdb;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import kong.unirest.core.GenericType;
-import mindsdb.models.Datasource;
 import mindsdb.models.Mind;
-import mindsdb.services.Datasources;
-import mindsdb.services.Minds;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 public class Main {
 
@@ -19,16 +8,21 @@ public class Main {
         String apiKey = "";
         String baseUrl = "https://staging.mdb.ai";
         MindsDb.init(apiKey, baseUrl);
-//        List<Mind> mindList = Minds.list();
-//        System.out.println(mindList);
-//        Mind mind = Minds.get("test");
-//        System.out.println(mind);
-        Optional<Datasource> datasource = Datasources.get("testds");
-        System.out.println(datasource.isEmpty());
-//        Minds.delete("test");
+//        Optional<List<Mind>> mindList = Minds.list();
+//        System.out.println(mindList.get());
+//        Optional<Mind> mind = Minds.get("newtest");
+//        System.out.println(mind.get());
+//        Optional<Datasource> datasource = Datasources.get("testds");
+//        System.out.println(datasource.isEmpty());
+//        boolean isDeleted = Minds.delete("newtestt");
+//        Datasource datasource = Datasource.
+        Mind mind = new Mind();
 //        Datasources.delete("testds");
+//        Datasources.delete("testds1");
 //        List<Datasource> datasources = Datasources.list();
 //        System.out.println(datasources);
+//        boolean isCreated = Minds.create("newtestt", Arrays.asList("testds"));
+//        System.out.println(isCreated);
 //        String s = "[{\"connection_data\":{\"database\":\"demo\",\"host\":\"samples.mindsdb.com\",\"integrations_name\":\"testds\",\"password\":\"demo_password\",\"port\":\"5432\",\"publish\":true,\"sampledb\":true,\"schema\":\"demo_data\",\"test\":true,\"type\":\"postgres\",\"user\":\"demo_user\",\"new-data\":[\"hi\",\"hee\",\"asf\"]},\"created_at\":\"Tue, 08 Oct 2024 01:12:37 GMT\",\"description\":\"Demo database\",\"engine\":\"postgres\",\"name\":\"testds\",\"tables\":[\"car_info\",\"car_sales\",\"financial_headlines\",\"home_rentals\",\"iris\",\"cta\",\"amazon_reviews\",\"review_sentiment\",\"chat_llm_mindsdb_docs\",\"house_sales\",\"used_car_price\",\"fraud_detection\",\"telecom_customer_churn\",\"crm_demo\",\"customer_churn\",\"bank_customer_transactions\",\"user_comments\",\"customer_support_chat\",\"jobs\"]}]";
 //        Type empTypeList = ;
 //        Gson gson = new Gson();
